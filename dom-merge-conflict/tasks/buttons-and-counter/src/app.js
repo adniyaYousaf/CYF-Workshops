aa8039d45f79a1ff8e0113f1d6ddfee223fd25ca
= <adniyayousaf92@gmail.com>
2024-05-11 12:47:55 +0100

    completed the task for person 1

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -4,13 +4,18 @@ function increment(node) {
   node.textContent = Number(current) + 1;
 }
 
+function decrement(node) {
+  let current = node.textContent;
+  node.textContent = Number(current) - 1;
+}
+
 export function App() {
   const body = document.createElement("body");
 
   const header = document.createElement("header");
   header.innerHTML = `
         <h1>Number Counter</h1>
-        <p>A simple counter. Press increment to increase the count by one.</p>
+        <p>A simple counter. Press increment and decrement to increase and decrease the count by one.</p>
     `;
   body.appendChild(header);
 
@@ -18,14 +23,20 @@ export function App() {
   main.innerHTML = `
         <p id="counter" data-testid="counter">0</p>
         <button id="increment">Increment</button>
+        <button id="decrement">decrement</button>
     `;
   body.appendChild(main);
 
   const button = body.querySelector("#increment");
+  const decrement = body.querySelector("#decrease");
+
   const counter = body.querySelector("#counter");
   button.addEventListener("click", () => {
     increment(counter);
   });
 
+  decrement.addEventListener("click", () => {
+    decrement(counter);
+  });
   return body;
 }
